477ea92fbf0262af02f989b036bfeb2825c8a30a
Odin <odinuv@gmail.com>
2016-08-03 17:50:45 +0200

    tests: fix missing composer install

--- tests.sh
+++ tests.sh
@@ -2,6 +2,7 @@
 echo "Starting tests" >&1
 php --version \
 	&& composer --version \
+	&& composer install \
  	&& /code/vendor/bin/phpcs --standard=psr2 -n --ignore=vendor --extensions=php . \
 
 mkdir /data/
